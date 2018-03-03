26ed7b065729699b63812e85a679c50495b7bef4
Fierid Neil <14002607+FieridNeil@users.noreply.github.com>
2018-03-02 19:03:06 -0800

    Update fruit.js

--- Snake Game/fruit.js
+++ Snake Game/fruit.js
@@ -1,8 +1,14 @@
+new p5;
 class Fruit{
   constructor(xpos, ypos){
     this.xpos = xpos;
     this.ypos = ypos;
     this.size = 10;
+    this.table = new p5.Table();
+    this.table.addColumn('iteration');
+    this.table.addColumn('xpos');
+    this.table.addColumn('ypos');
+    this.index = 1;
   }
 
   Show(){
@@ -20,11 +26,20 @@ class Fruit{
   }
 
   GenerateNew(){
-    var ran = [10, 20, 30, 40, 50];
-    this.xpos = this.RandInt(10, width) % 10 * ran[this.RandInt(0, 4)];
-    this.ypos = this.RandInt(10, height) % 10 * ran[this.RandInt(0, 4)];
-    console.log(this.xpos);
-    console.log(this.ypos);
+    if(this.index == 500){
+      console.log("Table is saved");
+      saveTable(this.table, 'new.csv');
+    }
+    var ran = [50, 40, 30, 50];
+    this.xpos = (this.RandInt(10, width-10) % 10) * ran[this.RandInt(0, ran.length - 1)];
+    this.ypos = (this.RandInt(10, height-10) % 10) * ran[this.RandInt(0, ran.length - 1)];
+
+
+    var newRow = this.table.addRow();
+    newRow.setNum('iteration', this.index);
+    newRow.setNum('xpos', this.xpos);
+    newRow.setNum('ypos', this.ypos);
+    this.index++;
   }
 
 }
